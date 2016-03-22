8026bdc131823d65f2e4ab63c8d90449f0d32273
saurabhj80 <saurabhj80@gmail.com>
2016-03-21 23:17:46 -0700

    Initial Commit

---
+++ Learning Core Image/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Core Image
+//
+//  Created by Saurabh Jain on 3/21/16.
+//  Copyright © 2016 Saurabh Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
